f1729fc9195b911188c71dd8b25f6945a46a3693
PawarSunil97 <pawarsunil281297@gmail.com>
2025-01-01 10:38:48 +0530

    added configration of image

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDE_NAME,
-    api_key:process.env.CLOUDE_API_KEY,
-    api_secret:process.env.CLOUDE_API_SECRTE
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
